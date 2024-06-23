2bbc2223e82b0b0e537b3e2f66c78bc0a7ab412c
hassan <hassanibrahimndahiro@gmail.com>
2024-06-23 14:21:48 +0300

    initial

---
+++ index.js
@@ -0,0 +1,4 @@
+function introductionWithLanguageOptional(name="Gracie" , language="JavaScript") {
+console.log(`Hi, my name is ${name} and i am learning to pragram in ${language}`)
+}
+ introductionWithLanguageOptional();
